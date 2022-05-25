842f7d640caf743a1f95dd0d35aae19b6e628532
kaine1 <kaine@onboardbase.com>
2022-05-25 15:08:37 +0100

    with useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
-
-// const App = () => {
-//   //create element creates an instance of an element e.g a div or a h1 or App
-//   return React.createElement("div", {}, [
-//     React.createElement(
-//       "h1",
-//       { id: "brand", className: "petPage ambassador" },
-//       "Adopt me please"
-//     ),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Thanos",
-//       animal: "Dog",
-//       breed: "CarneCorso",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Odin",
-//       animal: "Dog",
-//       breed: "Bull-dog",
-//     }),
-//   ]);
-// };
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-
-      <Pet name="Thanos" animal="Dog" breed="CarneCorso" />
-
-      <Pet name="Odin" animal="Dog" breed="Bull-dog" />
+      <SearchParams />
     </div>
   );
 };
-
-// const myRenderedApp = React.createElement(App);
-
-// render(myRenderedApp, document.getElementById("root"));
 
 render(<App />, document.getElementById("root"));
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,123 @@
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  //   const location = "Seattle, WA";
+  const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["poodle", "pichon"];
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = res.json();
+    setPets(json.pets);
+  }
+  //Hooks are called in the order they were created
+  //do not ever use conditional hooks because it will then have a variable amount of hooks being called
+  //and so a hook that comes after the conditional hook may be assigned the wrong thing
+  //basically dont use hooks in if statements or for-loops
+  // if (true) {
+  //   const [place, setPlace] = useState("");
+  // }
+
+  //   console.log(location);
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-    <div classname="search-params">
+    <div className="search-params">
+      {/* //controlled form where react monitors each change */}
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Location"
+          />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((thisBreed) => (
+              <option key={thisBreed} value={thisBreed}>
+                {thisBreed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
+
+      {pets.map((pet) => (
+        <Pet
+          key={pet.id}
+          name={pet.name}
+          animel={pet.animal}
+          breed={pet.breed}
+        />
+      ))}
+
+      {/* Uncontrolled form where the input is used only ion submit */}
+      {/* <form onSubmit={()}>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form> */}
+      {/* {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))} */}
     </div>
   );
 };
