7fe8e4d53fd7c7b6caa7e87db5ecf57ca44ae640
kaine1 <kaine@onboardbase.com>
2022-06-01 03:43:06 +0100

    with code splitting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 // import React from "react";
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import ThemeContext from "./ThemeContext";
-import Details from "./Details";
+// import Details from "./Details";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
+// import SearchParams from "./SearchParams";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("brown");
   return (
     <StrictMode>
-      <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeContext.Provider>
-      {/* <div>
+      <Suspense
+        fallback={
+          <h2 className="mx-auto animate-pulse">
+            This module is still loading...
+          </h2>
+        }
+      >
+        <ThemeContext.Provider value={theme}>
+          <BrowserRouter>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeContext.Provider>
+        {/* <div>
         
         
       </div> */}
+      </Suspense>
     </StrictMode>
   );
 };
